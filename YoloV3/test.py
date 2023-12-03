9758f25637f92e1bf7aa9e8e707f75b440ca01d2
unswimmingduck <857038459@qq.com>
2023-12-03 19:13:21 +0800

    test

--- YoloV3/test.py
+++ YoloV3/test.py
@@ -5,17 +5,17 @@ import torch
 
 def main(nimble=False):
     
-    model = Yolo_V3([[5,5,5],[5,5,5],[5,5,5]],5)
+    model = Yolo_V3([[5,5,5],[5,5,5],[5,5,5]],5).cuda()
     start_time = time.time()
     
     if nimble:
         model = torch.cuda.nimble(model)
-        model.prepare(torch.randr(1,3,416,416))
+        model.prepare(torch.randr(1,3,416,416).cuda(), training = True)
         prepare_time = time.time() - start_time
         print(f"nimble preparation time: {prepare_time}")
     
     start_time = time.time()
-    model(torch.randr(1,3,416,46))
+    model(torch.randr(1,3,416,46).cuda())
     train_time = time.time() - start_time
 
     if nimble:
